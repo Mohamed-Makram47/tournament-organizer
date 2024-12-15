1b8ffd8d7ac72e07eefc046c1cb9e4cf86bd3ff2
Ebrahim-Ramadan <mohamedmakram2323@gmail.com>
2024-12-15 04:25:56 +0200

    added shared layout on /tournaments

--- app/tournaments/[TournamentID]/page.tsx
+++ app/tournaments/[TournamentID]/page.tsx
@@ -1,8 +1,22 @@
-export default async function Page({
+import { redirect } from 'next/navigation'
+ 
+async function fetchTournamentData(TournamentID: string) {
+  const res = await fetch('https://...')
+  if (!res.ok) return undefined
+  return res.json()
+}
+ 
+export default async function Home({
   params,
 }: {
   params: Promise<{ TournamentID: string }>
 }) {
   const TournamentID = (await params).TournamentID
-  return <div>My tournament: {TournamentID}</div>
+  if (!TournamentID) {
+    redirect('/')
+  }
+ 
+  const team = await fetchTournamentData(TournamentID)
+ 
+  // ...
 }
---
+++ app/tournaments/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+     My Tournaments
+    </div>
+  );
+}
